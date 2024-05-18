26a0d1ce40b4393f008aa3c7ad17d35511512039
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2024-05-18 20:45:26 +0800

    Implement DrawBoard method, make constructors return a pointer to their types rather than their interface

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ func main() {
 	logger := log.New(os.Stdout, "gg: ", log.LstdFlags|log.Lshortfile)
 	in := NewStdinInput()
 	out := NewStdoutOutput()
-	gg := NewGG(logger, in, out)
+	gui := NewConsoleGUI(out)
+	gg := NewGG(logger, in, out, gui)
 
 	gg.Start()
 
@@ -31,8 +32,13 @@ func main() {
 // CONSTANTS
 // ==============================================================================
 const (
+	// Commands.
 	cmdInvalid = "invalid"
 	cmdExit    = "exit"
+
+	// Board dimensions.
+	rows  = 8
+	files = 9
 )
 
 // ==============================================================================
@@ -41,18 +47,43 @@ const (
 
 // GG is the game application instance.
 type GG struct {
-	logger         *log.Logger
+	// Game logic properties.
 	gameInProgress bool
-	in             Input
-	out            Output
+	board          GGBoard
+
+	// Ancillary dependencies.
+	logger *log.Logger
+	in     Input
+	out    Output
+	gui    GUI
 }
 
+// GGBoard is a 2D array for GGSquares.
+type GGBoard [rows][files]GGSquare
+
+// GGSquare represents a square on the game board.
+type GGSquare struct{}
+
 // NewGG initializes a new GG instance.
-func NewGG(logger *log.Logger, in Input, out Output) *GG {
+func NewGG(logger *log.Logger, in Input, out Output, gui GUI) *GG {
+	var board GGBoard
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < files; j++ {
+			board[i][j] = GGSquare{}
+		}
+	}
+
 	return &GG{
+		// Game logic properties.
+		gameInProgress: false,
+		board:          board,
+
+		// Ancillary dependencies.
 		logger: logger,
 		in:     in,
 		out:    out,
+		gui:    gui,
 	}
 }
 
@@ -72,6 +103,12 @@ func (g *GG) MainLoop() bool {
 	return g.gameInProgress
 }
 
+// DrawBoard displays a graphical representation of the current game state.
+func (g *GG) DrawBoard() {
+	g.logger.Println("drawing board.")
+	g.gui.Draw(g.board)
+}
+
 // GetCommand fetches the next player's command and invokes an appropriate handler.
 func (g *GG) GetCommand() {
 	g.logger.Println("fetching player command.")
@@ -88,11 +125,6 @@ func (g *GG) GetCommand() {
 	default:
 		g.HandleInvalid()
 	}
-}
-
-// DrawBoard displays a graphical representation of the current game state.
-func (g *GG) DrawBoard() {
-	g.logger.Println("drawing board.")
 }
 
 // DetermineResult calculates the game's result from the current game state.
@@ -149,7 +181,7 @@ func (i *StdinInput) Read() string {
 }
 
 // NewStdinInput initializes a new StdinInput.
-func NewStdinInput() Input {
+func NewStdinInput() *StdinInput {
 	return &StdinInput{}
 }
 
@@ -166,7 +198,72 @@ func (o *StdoutOutput) Write(s string) {
 	fmt.Print(s)
 }
 
+// GUI is the interface for handling interactable game elements.
+type GUI interface {
+	Draw(GGBoard)
+}
+
+// ConsoleGUI is a GUI implemented via console.
+type ConsoleGUI struct {
+	out *StdoutOutput
+}
+
+// NewConsoleGUI initializes a ConsoleGUI.
+func NewConsoleGUI(out *StdoutOutput) GUI {
+	return &ConsoleGUI{out: out}
+}
+
+func (g ConsoleGUI) Draw(board GGBoard) {
+	// Draw header
+	g.out.Write("\n")
+	g.out.Write(fmt.Sprintf("%s\n", strings.Repeat("=", 50)))
+	g.out.Write("Current game state:\n")
+
+	// Draw actual board.
+	for i := rows; i >= 0; i-- {
+		// Draw top edge.
+		for j := 0; j < files; j++ {
+			g.out.Write(" ----")
+		}
+		g.out.Write("\n")
+
+		// Draw each square.
+		for j := 0; j < files; j++ {
+			// TODO: We should only show per-square coordinates when setting up the board.
+			g.out.Write(fmt.Sprintf("| %s ", squareAddressToCoordinates(j, i)))
+		}
+		g.out.Write("|\n")
+
+		if i == 0 {
+			// Draw bottom edge.
+			for j := 0; j < files; j++ {
+				g.out.Write(" ----")
+			}
+			g.out.Write("\n")
+		}
+	}
+
+	// Draw footer
+	g.out.Write(fmt.Sprintf("%s\n", strings.Repeat("=", 50)))
+	g.out.Write("\n")
+}
+
 // NewStdoutOutput initializes a new StdoutOutput.
-func NewStdoutOutput() Output {
+func NewStdoutOutput() *StdoutOutput {
 	return &StdoutOutput{}
+}
+
+// ==============================================================================
+// Utility / helper functions.
+// ==============================================================================
+
+// squareAddressToCoordinates is a helper function to a square's index to its readable coordinate.
+// example: 03 -> A4, 57 -> F8.
+func squareAddressToCoordinates(x int, y int) string {
+	if x > rows || y > files {
+		return ""
+	}
+
+	alpha := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I"}
+	return fmt.Sprintf("%s%d", alpha[x], y+1)
 }
